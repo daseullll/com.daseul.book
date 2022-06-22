f077f38cb2b8c21d6a450da7beafdf08a2248a8f
안다슬 <dew4325@naver.com>
2022-06-23 02:37:45 +0900

    commit2

---
+++ src/main/java/com/daseul/book/springboot/web/HelloResponseDto.java
@@ -0,0 +1,12 @@
+package com.daseul.book.springboot.web;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class HelloResponseDto {
+
+    private final String name;
+    private final int amount;
+}
