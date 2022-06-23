d601345de2766eeda4ec2fad64efa2543e103098
안다슬 <dew4325@naver.com>
2022-06-24 01:06:52 +0900

    commit4

--- src/main/java/com/daseul/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/daseul/book/springboot/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.daseul.book.springboot.service.posts;
 
 import com.daseul.book.springboot.domain.posts.Posts;
 import com.daseul.book.springboot.domain.posts.PostsRepository;
+import com.daseul.book.springboot.web.dto.PostsListResponseDto;
 import com.daseul.book.springboot.web.dto.PostsResponseDto;
 import com.daseul.book.springboot.web.dto.PostsSaveRequestDto;
 import com.daseul.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -31,5 +35,12 @@ public class PostsService {
         Posts entity = postsRepository.findById(id).orElseThrow(()-> new IllegalArgumentException("해당 게시글이 없습니다. id="+ id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
